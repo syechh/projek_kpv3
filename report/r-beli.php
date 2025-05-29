6a0824ff3cbb4c60587e4c39265094bf68914e7d
Maulana Sandyy <akunpubg91723@gmail.com>
2025-05-29 15:07:34 +0700

    update struk

--- report/r-beli.php
+++ report/r-beli.php
@@ -22,10 +22,14 @@ $dataBeli = getData("SELECT * FROM beli_head WHERE tgl_beli BETWEEN '$tgl1' AND 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Laporan Pembelian</title>
+    <style>
+        table { width: 100%; border-collapse: collapse; }
+        th, td { padding: 6px; }
+        .barang-row td { padding-left: 30px; font-size: 14px; }
+        .grey-line { margin-bottom: 2px; margin-left: -5px; margin-top: 1px; }
+    </style>
 </head>
 <body>
-    
-
     <div style="text-align: center;">
         <h2 style="margin-bottom: -15px">Rekap Laporan Pembelian</h2>
         <h2 style="margin-bottom: 15px">Toko Bangunan Mutiara</h2>
@@ -35,7 +39,7 @@ $dataBeli = getData("SELECT * FROM beli_head WHERE tgl_beli BETWEEN '$tgl1' AND 
         <thead>
             <tr>
                 <td colspan="5" style="height: 5px;">
-                    <hr style="margin-bottom: 2px; margin-left: -5px;", size="3", color="grey">
+                    <hr class="grey-line" size="3" color="grey">
                 </td>
             </tr>
             <tr>
@@ -47,15 +51,14 @@ $dataBeli = getData("SELECT * FROM beli_head WHERE tgl_beli BETWEEN '$tgl1' AND 
             </tr>
             <tr>
                 <td colspan="5" style="height: 5px;">
-                    <hr style="margin-bottom: 2px; margin-left: -5px; margin-top: 1px;", size="3", color="grey">
+                    <hr class="grey-line" size="3" color="grey">
                 </td>
             </tr>
         </thead>
         <tbody>
             <?php
-                $no = 1;
-                foreach ($dataBeli as $data) { ?>
-
+            $no = 1;
+            foreach ($dataBeli as $data) { ?>
                 <tr>
                     <td><?= $no++ ?></td>
                     <td align="center"><?= in_date($data['tgl_beli']) ?></td>
@@ -64,14 +67,22 @@ $dataBeli = getData("SELECT * FROM beli_head WHERE tgl_beli BETWEEN '$tgl1' AND 
                     <td align="right"><?= number_format($data['total'],0,',','.') ?></td>
                 </tr>
 
-            <?php
-            }
-            ?>
+                <?php
+                $detailBrg = getData("SELECT bd.*, b.satuan FROM beli_detail bd LEFT JOIN barang b ON bd.kode_brg = b.id_barang WHERE bd.no_beli = '{$data['no_beli']}'");
+                foreach ($detailBrg as $barang) { ?>
+                    <tr style="background-color: #f9f9f9; font-size: 12px;">
+                        <td></td>
+                        <td colspan="2"><?= htmlspecialchars($barang['nama_brg']) ?> (<?= $barang['qty'] ?> <?= htmlspecialchars($barang['satuan'] ?? '-') ?>)</td>
+                        <td colspan="2" align="right">Rp <?= number_format($barang['harga_beli'],0,',','.') ?></td>
+                    </tr>
+                <?php } 
+            } ?>
         </tbody>
+
         <tfoot>
             <tr>
                 <td colspan="5" style="height: 5px;">
-                    <hr style="margin-bottom: 2px; margin-left: -5px; margin-top: 1px;", size="3", color="grey">
+                    <hr class="grey-line" size="3" color="grey">
                 </td>
             </tr>
         </tfoot>
@@ -80,13 +91,5 @@ $dataBeli = getData("SELECT * FROM beli_head WHERE tgl_beli BETWEEN '$tgl1' AND 
     <script>
         window.print();
     </script>
-
-
 </body>
 </html>
-
-
-<?php
-
-
-?>
