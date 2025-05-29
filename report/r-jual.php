e07977a55b71a3cf946495b824354f6f78daf44c
Maulana Sandy <maulanasandy.official@gmail.com>
2025-05-29 10:50:00 +0700

    menambahkan jumlah pendapatan pada report struk

--- report/r-jual.php
+++ report/r-jual.php
@@ -43,7 +43,7 @@ $dataJual = getData("SELECT * FROM jual_head WHERE tgl_jual BETWEEN '$tgl1' AND 
                 <th style="width: 120px;">Tgl Penjualan</th>
                 <th style="width: 120px;">Id Penjualan</th>
                 <th style="width: 300px;">Customer</th>
-                <th>Total Pemjualan</th>
+                <th>Total Penjualan</th>
             </tr>
             <tr>
                 <td colspan="5" style="height: 5px;">
@@ -54,20 +54,33 @@ $dataJual = getData("SELECT * FROM jual_head WHERE tgl_jual BETWEEN '$tgl1' AND 
         <tbody>
             <?php
                 $no = 1;
-                foreach ($dataJual as $data) { ?>
-
-                <tr>
-                    <td><?= $no++ ?></td>
-                    <td align="center"><?= in_date($data['tgl_jual']) ?></td>
-                    <td align="center"><?= $data['no_jual'] ?></td>
-                    <td align="center"><?= $data['customer'] ?></td>
-                    <td align="right"><?= number_format($data['total'],0,',','.') ?></td>
-                </tr>
-
+                $totalPendapatan = 0;
+                foreach ($dataJual as $data) {
+                $totalPendapatan += $data['total'];
+            ?>
+            <tr>
+                <td><?= $no++ ?></td>
+                <td align="center"><?= in_date($data['tgl_jual']) ?></td>
+                <td align="center"><?= $data['no_jual'] ?></td>
+                <td align="center"><?= $data['customer'] ?></td>
+                <td align="right"><?= number_format($data['total'],0,',','.') ?></td>
+            </tr>
             <?php
-            }
+                }
             ?>
         </tbody>
+        <tfoot>
+            <tr>
+                <td colspan="5" style="height: 5px;">
+                    <hr style="margin-bottom: 2px; margin-left: -5px; margin-top: 1px;" size="3" color="grey">
+                </td>
+            </tr>
+            <tr>
+                <th colspan="4" align="right">Total Penjualan:</th>
+                <th align="right"><?= number_format($totalPendapatan, 0, ',', '.') ?></th>
+            </tr>
+        </tfoot>
+
         <tfoot>
             <tr>
                 <td colspan="5" style="height: 5px;">
